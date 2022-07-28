9bab271b0b8bf864f3a2645b5f9901782d0688d6
Bohdan Ses <bohdan.ses@gmail.com>
2022-07-28 20:59:56 +0200

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(galleryItem => {
+        return `
+        <li>
+            <a class="gallery__item" href="${galleryItem.original}">
+                <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}"/>
+            </a>
+        </li> 
+    `;
+    }).join("");
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: "alt"});
+gallery.on('show.simplelightbox');

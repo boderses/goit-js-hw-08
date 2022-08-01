6baae4cf17034bcf5d550db3664c057ed5fee8c1
Bohdan Ses <bohdan.ses@gmail.com>
2022-08-01 20:51:00 +0200

    task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.on("loaded", onPageReload);
+   
+function onTimeUpdate(){
+        player.getCurrentTime().then(time => localStorage.setItem("videoplayer-current-time", time));
+}
+
+function onPageReload() {
+        const currentTime = localStorage.getItem("videoplayer-current-time");
+        player.setCurrentTime(currentTime).catch((error) => {
+                switch (error.name) {
+                        case 'RangeError':
+                                break;
+                }
+        });
+}

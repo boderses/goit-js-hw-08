b0f1e582409b07087bd9194d08aefbc87ccc0cb4
Bohdan Ses <bohdan.ses@gmail.com>
2022-08-09 19:31:31 +0200

    task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+onPageLoad();
+
+feedbackForm.addEventListener("input", throttle(addDataToLocalStorage, 500));
+feedbackForm.addEventListener("submit", onFormSubmit);
+
+const onInput = () => ({
+    email: feedbackForm.elements.email.value,
+    message: feedbackForm.elements.message.value,
+});
+    
+function addDataToLocalStorage() {
+    const inputData = onInput();
+    localStorage.setItem("feedback-form-state", JSON.stringify(inputData));
+}
+
+function onPageLoad() {
+    const savedData = localStorage.getItem("feedback-form-state");
+    if (savedData) {
+        const parsedData = JSON.parse(savedData);
+        feedbackForm.elements.email.value = parsedData.email;
+        feedbackForm.elements.message.value = parsedData.message
+    }
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(onInput());
+    localStorage.clear();
+    feedbackForm.reset();
+}
